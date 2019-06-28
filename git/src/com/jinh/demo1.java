4da444c174c84905e424544a55e0c3e97c42d1f5
jinh-git <1292440426@qq.com>
2019-06-28 16:46:10 +0800

    test

---
+++ git/src/com/jinh/demo1.java
@@ -0,0 +1,12 @@
+package com.jinh;
+
+public class demo1 {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
